c603a9a7815191510edf501596d69a557afd411f
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-10-29 16:52:54 +0100

    optimized in rust

---
+++ names.rs
@@ -0,0 +1,32 @@
+fn main() {
+println!("# Names");
+
+    let names = [
+        "Amy Pattanasethanon",
+        "Anne-Sofie Elliot",
+        "Daniel Mootis",
+        "Elaynne Liman",
+        "George Lucas Serelis",
+        "Haden Chambles",
+        "Hands Gerwinkle",
+        "Jann Ostrokra",
+        "Juananan Ruitiña",
+        "Koala Ojoodidn't",
+        "Laia Ruffunni",
+        "Lidia Loona Puertchka",
+        "Luybimor Popoff",
+        "Mattee-ah Turick",
+        "Maxibillion Blazin'",
+        "Migelle Divvoh",
+        "Niña Roorke",
+        "Peter Pogo-stick",
+        "Piper Donk",
+        "Sofia Feldrin",
+        "Time \"waits for no one\" Udvarhelyi"
+    ];
+
+    for name in names.iter() {
+        println!("{}", name);
+    }
+
+}
